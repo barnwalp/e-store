53aeae22ba8fbdc8fff43f0fada5f7f715905365
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-10-04 08:10:01 +0530

    bug/added a publicroute in the middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // with this middleware, entire application is protected. if you
 // try to access it, middleware will redirect you to your sign Up page.
-export default authMiddleware({});
+export default authMiddleware({
+	publicRoutes: ["/"]
+});
  
 export const config = {
       matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
