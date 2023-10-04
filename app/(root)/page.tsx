9bd76a0963d63b154c9840a3f1909aade9bc740b
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-10-04 07:13:18 +0530

    feat/added authentication using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Page() {
 	return (
 		<div>
 			<h1>Page</h1>
+			<Button>Test</Button>
+			<UserButton afterSignOutUrl="/" />
 		</div>
 	);
 }
---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+// with this middleware, entire application is protected. if you
+// try to access it, middleware will redirect you to your sign Up page.
+export default authMiddleware({});
+ 
+export const config = {
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
