6a813c45aab0b9842c98dc106f1e8df2b23e19d6
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-10-07 13:27:38 +0530

    bug/re-arranged files to start sign-in pages

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Page() {
+export default function Home() {
 	return (
 		<div>
 			<h1>Page</h1>
 			<Button>Test</Button>
-			{/*}<UserButton afterSignOutUrl="/" />{*/}
+			<UserButton afterSignOutUrl="/" />
 		</div>
 	);
 }
