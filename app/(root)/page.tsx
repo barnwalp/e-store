64e4c702963746e11be7cb210ad677ea6d5cdee1
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-11-01 07:02:03 +0530

    feat/added root page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from 'next/link'
+
+
+export default function Home() {
+	return (
+		<div className="ml-6 mt-4">
+			<h1>Root Page</h1>
+			<Button variant="default">Keep Practicing</Button>
+			<Link className={buttonVariants({variant: 'outline'})}  href="#">Click Here</Link>
+		</div>
+	)
+}
