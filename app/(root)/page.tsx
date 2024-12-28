4940032eef8c8cd98b91c66016e8496905bb8445
Pankaj Barnwal <suno.pankaj@gmail.com>
2024-12-28 12:12:45 +0530

    add feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { DialogModal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 import Link from 'next/link'
-
+import { useStore } from "zustand";
 
 export default function Home() {
+	const bears: { bears: number } = useStore((state) => state.bears)
 	return (
 		<div className="ml-6 mt-4">
 			<h1>Root Page</h1>
@@ -12,6 +13,7 @@ export default function Home() {
 			<Link className={buttonVariants({variant: 'outline'})}  href="#">Click Here</Link>
 			<UserButton afterSignOutUrl="/" />
 			<DialogModal title="E Commerce Store" description="Best Store in the World" />
+			<h3>{ bears } around here</h3>
 		</div>
 	)
 }
